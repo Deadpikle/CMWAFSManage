d1b72e91836f735b4ae85c826ab4ca5132c5efb1
Deadpikle <mikebobby@sbcglobal.net>
2018-06-24 19:34:23 -0400

    Initial commit

---
+++ CMWAFSManager/CMWAFSManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CMWAFSManager
+//
+//  Created by School of Computing Macbook on 6/24/18.
+//  Copyright © 2018 Pikle Productions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
