d227fc991463c46919fc274d1372b214480fee15
Deadpikle <mikebobby@sbcglobal.net>
2018-06-24 20:11:59 -0400

    Start fleshing out Room model

---
+++ CMWAFSManager/CMWAFSManager/Models/Room.h
@@ -0,0 +1,21 @@
+//
+//  Room.h
+//  CMWAFSManager
+//
+//  Created by School of Computing Macbook on 6/24/18.
+//  Copyright © 2018 Pikle Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+@interface Room : NSObject
+
+@property NSUInteger roomID;
+@property NSString *buildingName;
+@property NSUInteger number;
+@property NSUInteger maxNumberOfPeople;
+
+@property NSColor *color;
+
+@end
---
+++ CMWAFSManager/CMWAFSManager/Models/Room.m
@@ -0,0 +1,13 @@
+//
+//  Room.m
+//  CMWAFSManager
+//
+//  Created by School of Computing Macbook on 6/24/18.
+//  Copyright © 2018 Pikle Productions. All rights reserved.
+//
+
+#import "Room.h"
+
+@implementation Room
+
+@end
