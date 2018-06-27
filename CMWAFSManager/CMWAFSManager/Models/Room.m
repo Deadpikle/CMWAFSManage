b6b5363cd61dc796249efe6c60ae4558f00e147f
Deadpikle <mikebobby@sbcglobal.net>
2018-06-26 20:50:54 -0400

    First pass at insert room into db

--- CMWAFSManager/CMWAFSManager/Models/Room.m
+++ CMWAFSManager/CMWAFSManager/Models/Room.m
@@ -8,15 +8,37 @@
 
 #import "Room.h"
 #import "DBHelper.h"
+#import "Utilities.h"
 
 @implementation Room
 
 -(void)insertIntoDatabase {
     // TODO:
     // create INSERT INTO Rooms (...) string
-    // grab database
-    // insert into database
-    // return YES/NO for success/failure
+    FMDatabase *database = [DBHelper openDatabase];
+    if (database) {
+        NSString *insert = @" \
+            INSERT INTO Rooms (BuildingName, Number, MaxPeople, Color) \
+            VALUES (?, ?, ?, ?);";
+        NSString *colorString = [Utilities convertColorToString:self.color];
+        if (!colorString) {
+            colorString = @"#FFFFFF";
+        }
+        NSError *error = nil;
+        [database executeUpdate:insert
+                         values:@[self.buildingName,
+                                  [NSNumber numberWithUnsignedInteger:self.number],
+                                  [NSNumber numberWithUnsignedInteger:self.maxNumberOfPeople],
+                                  colorString]
+                          error:&error];
+        if (error) {
+            NSLog(@"We failed to add a room!");
+            // TODO:
+        }
+        // otherwise it's in the database. yay!
+        NSLog(@"Successfully added a room!");
+    }
+    // TODO: return YES/NO for success/failure
 }
 
 +(NSArray<Room*>*)loadAllRooms {
